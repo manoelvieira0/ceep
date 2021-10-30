8c93ecd1dfba05df628ab31354124d818fbcf12b
netovieira01_ <netinhovieira1801@gmail.com>
2021-10-30 07:47:23 -0300

    Definindo Comportamento do formulário

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -1,6 +1,7 @@
 const novaTarefa = document.querySelector('[data-form-button')                        /* Percorrer o DOM */
 
-novaTarefa.addEventListener('click', () => {
+novaTarefa.addEventListener('click', (evento) => {
+    evento.preventDefault()
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
     console.log(valor)
