92c439fc0ca615aab9c94a40670a5ca54fe85d94
netovieira01_ <netinhovieira1801@gmail.com>
2021-11-04 07:18:58 -0300

    Removendo a tarefa

--- listaDeTarefas.js
+++ listaDeTarefas.js
@@ -9,6 +9,7 @@
         const conteudo = `<p class="content">${valor}</p>`
         tarefa.innerHTML = conteudo
         tarefa.appendChild(BotaoConclui())
+        tarefa.appendChild(BotaoDeleta())
         lista.appendChild(tarefa)
         input.value = ''
     }
@@ -29,6 +30,20 @@
         const botaoConclui = evento.target
         const tarefaCompleta = botaoConclui.parentElement
         tarefaCompleta.classList.toggle('done')
+    }
+
+    const BotaoDeleta = () => {
+        const botaoDeleta = document.createElement('button')
+        botaoDeleta.innerText = 'deletar'
+        botaoDeleta.addEventListener('click', deletarTarefa)
+        return botaoDeleta
+    }
+
+    const deletarTarefa = (evento) => {
+        const botaoDeleta = evento.target
+        const tarefaCompleta = botaoDeleta.parentElement
+        tarefaCompleta.remove()
+        return botaoDeleta
     }
 }
 )()
