21c775a2e4677c8baf61fb6466bd711cf0904e94
netovieira01_ <netinhovieira1801@gmail.com>
2021-11-29 07:25:42 -0300

    Campo nova data

--- main.js
+++ main.js
@@ -2,14 +2,23 @@ import BotaoConclui from "./componentes/concluiTarefa.js"
 import BotaoDeleta from "./componentes/deletaTarefa.js"
 
     const criarTarefa = (evento) => {
+
         evento.preventDefault()
+
         const lista = document.querySelector('[data-list]')
         const input = document.querySelector('[data-form-input]')
         const valor = input.value
+
+        const calendario = document.querySelector('[data-form-date]')
+        const data = calendario.value
+        console.log(data)
+ 
         const tarefa = document.createElement('li')
         tarefa.classList.add('task')
         const conteudo = `<p class="content">${valor}</p>`
+
         tarefa.innerHTML = conteudo
+        
         tarefa.appendChild(BotaoConclui())
         tarefa.appendChild(BotaoDeleta())
         lista.appendChild(tarefa)
